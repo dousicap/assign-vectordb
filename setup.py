ad63bbef622c89e2a9e69389eba128d1b1855a59
dou <papamadouba@gmail.com>
2025-08-24 22:56:08 +0200

    updated 2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="assign_vectordb",
+    version="0.0.0",
+    author="Dou",
+    author_email="papamadouba@gmail.com",
+    packages=find_packages()
+)
